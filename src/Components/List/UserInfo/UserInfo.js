8a39a9fd69adc0ada4c91ebc04616d73b4a7b8c7
Abhimanyu Ahuja <abhimanyuahuja12.com>
2024-07-01 05:13:14 +0530

    add basic structure to list

--- src/Components/List/UserInfo/UserInfo.js
+++ src/Components/List/UserInfo/UserInfo.js
@@ -4,11 +4,16 @@ import "./userinfo.css";
 function UseInfo() {
   return (
     <div className='userinfo'>
-      <div className="user"></div>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>john doe</h2>
+      </div>
+
+      
       <div className="icons">
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+        <img src="./more.png" alt="" />
+        <img src="./edit.png" alt="" />
+        <img src="./video.png" alt="" />
       </div>
     </div>
   )
