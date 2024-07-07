9fa5592c9157aba661d9d5771305c656f2486e1f
Abhimanyu Ahuja <abhimanyuahuja12@gmail.com>
2024-07-08 02:30:28 +0530

    change UI of ChatList component

--- src/Components/List/ChatList/ChatList.js
+++ src/Components/List/ChatList/ChatList.js
@@ -1,13 +1,53 @@
 import React from 'react'
 import "./chatList.css";
+import { useState } from 'react';
 
-function chatList() {
+const ChatList=() =>{
+
+  const [addMode , setaddMode]= useState(false)
   return (
     <div
     className='chatlist'>
-      chatlist
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder='search here' />
+        </div>
+        <img src={addMode ? "./minus.png":"./plus.png" }alt="" className='add'
+        onClick={()=>setaddMode((prev)=>!prev)}/>
+      
+      </div>
+
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>john Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>john Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>john Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>john Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default chatList
+export default ChatList;
