1f5c89040fe12ef4946d9a09362866c1f6a18147
Abhimanyu Ahuja <abhimanyuahuja12@gmail.com>
2024-07-10 10:21:33 +0530

    Updated the chat component

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import EmojiPicker from "emoji-picker-react";
+
 import "./chat.css"
-function Chat() {
+const Chat=()=> {
+
+const[open,setopen]=useState(false);
+
   return (
     <div className='chat'>
-      Chat
+
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>John Doe</span>
+            <p>Lorem ipsum dolor sit, amet consectetur adi</p>
+          </div>
+        </div>
+
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+
+      </div>
+
+      
+
+      <div className="center"></div>
+
+
+
+
+      <div className="bottom">
+
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+
+        <input type="text" placeholder='Type a message..' />
+        <div className="emoji">
+          <img src="./emoji.png" alt="" 
+          onClick={()=>setopen((prev)=>!prev)}/>
+          <EmojiPicker open={open}/>
+        </div>
+
+        <button className='sendButton'>Send</button>
+      </div>
     </div>
   )
 }
